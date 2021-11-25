7ee39d98d9b4bb3c8f710f546ef8806ae11eca76
oded <oded86@gmail.com>
2021-11-25 22:58:51 +0200

    Changed Codec

--- recorder.py
+++ recorder.py
@@ -3,6 +3,7 @@ import time
 import sys
 import numpy as np
 
+
 class dogRecorder:
     def __init__(self, video_url, video_length, ticket_id):
         self.video_url = video_url
@@ -15,14 +16,14 @@ class dogRecorder:
             print("!!! Failed VideoCapture: invalid parameter!")
         now = time.time()
         future = now + self.video_length
-        frame_width = int(vidcap.get(3)* 0.5)
-        frame_height = int(vidcap.get(4)* 0.5)
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
-        video_file_name = 'outpy_' + str(self.ticket_id) + '.mp4'
+        frame_width = int(vidcap.get(3) * 0.5)
+        frame_height = int(vidcap.get(4) * 0.5)
+        fourcc = cv2.VideoWriter_fourcc('V', 'P', '0', '9')
+        video_file_name = 'outpy_' + str(self.ticket_id) + '.webm'
         out = cv2.VideoWriter(video_file_name, fourcc, 10, (frame_width, frame_height))
         while time.time() < future:
             ret, frame = vidcap.read()
-            if ret == True:
+            if ret:
                 resized = cv2.resize(frame, (frame_width, frame_height), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
                 out.write(resized)
 
@@ -32,5 +33,5 @@ class dogRecorder:
         return video_file_name
 
 
-#rc = dogRecorder('rtsp://admin:Champi0n%24@2.55.114.241:8554/1', 10, 1)
+#rc = dogRecorder('rtsp://admin:Champi0n%24@2.55.114.241:8554/1', 30, 1)
 #rc.record_movie()
